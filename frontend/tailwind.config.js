a1f417aa5e424ff9e0fefee6f099061286a8ed17
Dan Nguyen <nguyenphungmaidan@gmail.com>
2025-03-18 10:05:31 +0700

    Update .gitignore to include .env.docker and format tailwind.config.js for consistency

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/views/**/*.{html,js}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/views/**/*.{html,js}'
   ],
   theme: {
     screens: {
-      sm: { max: "639px" },
+      sm: { max: '639px' },
 
-      md: { max: "767px" },
+      md: { max: '767px' },
 
-      lg: { max: "1023px" },
+      lg: { max: '1023px' },
 
-      xl: { max: "1279px" },
+      xl: { max: '1279px' }
     },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
